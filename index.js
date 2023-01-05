6341ee693788e38e10466920814f6d93b8571682
iabdukhoshimov <i.abdukhoshimov@gmail.com>
2023-01-05 23:27:39 +0500

    add: secret keys

--- index.js
+++ index.js
@@ -1,57 +1,43 @@
-const express = require("express");
-const { google } = require("googleapis");
+const { google } = require('googleapis');
 
-const app = express();
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/", (req, res) => {
-    res.render("index");
-});
-
-app.post("/", async(req, res) => {
-    const { request, name } = req.body;
-
+;
+(async function() {
     const auth = new google.auth.GoogleAuth({
         keyFile: "credentials.json",
         scopes: "https://www.googleapis.com/auth/spreadsheets",
     });
 
-    // Create client instance for auth
     const client = await auth.getClient();
 
-    // Instance of Google Sheets API
-    const googleSheets = google.sheets({ version: "v4", auth: client });
+    const googleSheets = google.sheets({ version: "v4", auth: client })
 
-    const spreadsheetId = "1J5OesnSTJCgLTTA0hQ_QSk_UPVK1nwRTEkvvRHHrEqM";
+    const spreadsheetId = "1fJ5htPSqVeVGTbVw8u57mbGLw-aZ9QYghnneGxbh53o";
 
-    // Get metadata about spreadsheet
     const metaData = await googleSheets.spreadsheets.get({
         auth,
         spreadsheetId,
     });
 
-    // Read rows from spreadsheet
+
+    // selecting rows
     const getRows = await googleSheets.spreadsheets.values.get({
         auth,
         spreadsheetId,
-        range: "Sheet1!A:A",
+        range: "Лист1",
     });
 
-    // Write row(s) to spreadsheet
-    await googleSheets.spreadsheets.values.append({
+
+    // adding rows
+    const data = await googleSheets.spreadsheets.values.append({
         auth,
         spreadsheetId,
-        range: "Sheet1!A:B",
+        range: "Лист1!A:C",
         valueInputOption: "USER_ENTERED",
         resource: {
             values: [
-                [request, name]
+                ['2', 'Madiyor', 'Abdukhoshimov']
             ],
         },
     });
 
-    res.send("Successfully submitted! Thank you!");
-});
-
-app.listen(1337, (req, res) => console.log("running on 1337"));
+})()
